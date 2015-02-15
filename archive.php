78bc2828e9f7dd2e0803a4a23842f3c90828262c
Steffen Arnold <st.arnold.84@googlemail.com>
2015-02-15 15:27:01 +0100

    syntax documentation

--- archive.php
+++ archive.php
@@ -29,71 +29,7 @@ get_header(); ?>
 	<div id="main" class="site-main" role="main">
             <?php do_action( 'rootstrap_post_before' ); ?>
             <?php if ( have_posts() ) : ?>
-
-                <header class="page-header">
-                    <h1 class="page-title">
-                        <?php
-                            if ( is_category() ) :
-                                single_cat_title();
-
-                            elseif ( is_tag() ) :
-                                single_tag_title();
-
-                            elseif ( is_author() ) :
-                                printf( __( 'Author: %s', 'rootstrap' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-                            elseif ( is_day() ) :
-                                printf( __( 'Day: %s', 'rootstrap' ), '<span>' . get_the_date() . '</span>' );
-
-                            elseif ( is_month() ) :
-                                printf( __( 'Month: %s', 'rootstrap' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'rootstrap' ) ) . '</span>' );
-
-                            elseif ( is_year() ) :
-                                printf( __( 'Year: %s', 'rootstrap' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'rootstrap' ) ) . '</span>' );
-
-                            elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-                                _e( 'Asides', 'rootstrap' );
-
-                            elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-                                _e( 'Galleries', 'rootstrap');
-
-                            elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-                                _e( 'Images', 'rootstrap');
-
-                            elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-                                _e( 'Videos', 'rootstrap' );
-
-                            elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-                                _e( 'Quotes', 'rootstrap' );
-
-                            elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-                                _e( 'Links', 'rootstrap' );
-
-                            elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-                                _e( 'Statuses', 'rootstrap' );
-
-                            elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-                                _e( 'Audios', 'rootstrap' );
-
-                            elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-                                _e( 'Chats', 'rootstrap' );
-
-                            else :
-                                _e( 'Archives', 'rootstrap' );
-
-                            endif;
-                        ?>
-                    </h1>
-                    <?php
-                        // Show an optional term description.
-                        $term_description = term_description();
-                        if ( ! empty( $term_description ) ) :
-                            printf( '<div class="taxonomy-description">%s</div>', $term_description );
-                        endif;
-                    ?>
-                </header><!-- .page-header -->
-
-                <?php /* Start the Loop */ ?>
+              <?php /* Start the Loop */ ?>
                 <?php while ( have_posts() ) : the_post(); ?>
 
                     <?php
