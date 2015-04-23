5d71de3cceb18d3ac19eca26bcc651f06cb301e0
René Hermenau <admin@xsimulator.net>
2015-04-23 19:59:14 +0200

    faq

--- page-faq.php
+++ page-faq.php
@@ -17,30 +17,22 @@ Template Name: page-faq
             <h2>Read the FAQ first before <br>you  <a title="Mashshare support" href="<?php echo get_permalink('93'); ?>">open a ticket.</a></h2>
         </div>
         <div class="col-md-5">
-            <img src="https://mashshare.net/dev2/wp-content/themes/wp-rootstrap/images/packshot-faq.png" alt="Packshot FAQ" style="margin-bottom:30px;"/>
+            <img src="https://mashshare.net/dev2/wp-content/themes/wp-rootstrap/images/packshot-faq.png" alt="Packshot FAQ" style="margin-bottom:30px;"/> 
         </div>
     </div><!--end row-->
 </div><!--end header-->
 </div><!--end header-image-->
 <div class="decor-top container"><img src="https://mashshare.net/dev2/wp-content/uploads/2014/12/decor_top.png" alt="blue" /></div>
             <div class="row"><!-- start row -->
-		<div id="content" class="col-xs-12 col-md-offset2 col-md-10 col-lg-offset2 col-lg-10">
-                    <div class="container"><!-- start container -->
+		<div class="col-xs-12 col-md-offset-3 col-md-6 col-lg-offset-3 col-lg-6">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
 				<div class="entry">
-	
 					<?php the_content(); ?>
-	
 					<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-	
 				</div>
-	
                         </article>
 			<?php endwhile; endif; ?>
-                    </div><!-- end container -->
 		</div> <!-- end content -->
             </div><!-- end row -->
 	</div> <!-- end page-wrap -->
