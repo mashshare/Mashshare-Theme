8a763fcdc179969e4f77aaaaac4cb3aff8664935
Steffen Arnold <st.arnold.84@googlemail.com>
2015-04-23 19:54:58 +0200

    page-bundle

--- page-bundle.php
+++ page-bundle.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: page-faq
+Template Name: page-bundle
 */
 ?>
 
@@ -12,34 +12,43 @@ Template Name: page-faq
             <div id="header-image"><!--start header-image-->
 <div id="header-content" class="container"><!--start header-->
     <div class="row"><!--start row-->
-        <div class="col-md-7">
-            <h1>Frequently <br>Asked Questions</h1>
-            <h2>Read the FAQ first before <br>you  <a title="Mashshare support" href="<?php echo get_permalink('93'); ?>">open a ticket.</a></h2>
-        </div>
-        <div class="col-md-5">
-            <img src="https://mashshare.net/dev2/wp-content/themes/wp-rootstrap/images/packshot-faq.png" alt="Packshot FAQ" style="margin-bottom:30px;"/>
+        <div class="col-md-12" style="text-align: center;">
+            <h1>30 Day Money Back Guarantee</h1>
+            <h2>We stand behind our product 100% – see our refund policy</h2>
         </div>
     </div><!--end row-->
 </div><!--end header-->
 </div><!--end header-image-->
 <div class="decor-top container"><img src="https://mashshare.net/dev2/wp-content/uploads/2014/12/decor_top.png" alt="blue" /></div>
             <div class="row"><!-- start row -->
-		<div id="content" class="col-xs-12 col-md-offset2 col-md-10 col-lg-offset2 col-lg-10">
+		<div id="content" class="col-md-12 col-lg-12 col-xs-12">
                     <div class="container"><!-- start container -->
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-				<div class="entry">
-	
-					<?php the_content(); ?>
-	
-					<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-	
-				</div>
-	
-                        </article>
-			<?php endwhile; endif; ?>
+                        <div class="row">
+                            <div class="bundle first col-md-4 col-xs-4">
+                                <h6>Personal</h6>
+                                <p>1 Site</p>
+                                <p>&euro;39</p>
+                                <p>1 Year of Updates & Support**</p>
+                                <p>30 Days Satisfied or Refunded</p>
+                                <p>10 professional add-ons</p>
+                            </div>
+                            <div class="bundle second col-md-4 col-xs-4">
+                                <h6>Business</h6>
+                                <p>3 Site</p>
+                                <p>&euro;99</p>
+                                <p>1 Year of Updates & Support**</p>
+                                <p>30 Days Satisfied or Refunded</p>
+                                <p>10 professional add-ons</p>
+                            </div>
+                            <div class="bundle third col-md-4 col-xs-4">
+                                <h6>Pro</h6>
+                                <p>1 Site</p>
+                                <p>&euro;199</p>
+                                <p>1 Year of Updates & Support**</p>
+                                <p>30 Days Satisfied or Refunded</p>
+                                <p>10 professional add-ons</p>
+                            </div>
+                        </div>
                     </div><!-- end container -->
 		</div> <!-- end content -->
             </div><!-- end row -->
