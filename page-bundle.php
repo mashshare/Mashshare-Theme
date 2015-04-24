2f5668e7fa5bb5ba05336e1317555ac1c7e44988
Steffen Arnold <st.arnold.84@googlemail.com>
2015-04-24 12:00:42 +0200

    page-budnle.php

--- page-bundle.php
+++ page-bundle.php
@@ -22,36 +22,50 @@ Template Name: page-bundle
 <div class="decor-top container"><img src="https://mashshare.net/dev2/wp-content/uploads/2014/12/decor_top.png" alt="blue" /></div>
             <div class="row"><!-- start row -->
 		<div id="content" class="col-md-12 col-lg-12 col-xs-12">
-                    <div class="container"><!-- start container -->
-                        <div class="row">
-                            <div class="bundle first col-md-4 col-xs-4">
-                                <h6>Personal</h6>
-                                <p class="price">1 Site</p>
-                                <p class="price">&euro;39</p>
-                                <p>1 Year of Updates & Support**</p>
-                                <p>30 Days Satisfied or Refunded</p>
-                                <p>10 professional add-ons</p>
-                                <p><?php echo do_shortcode( '[purchase_link id="21330" text="Buy Now" style="button" direct="true"]' ) ?></p>
-                            </div>
-                            <div class="bundle second col-md-4 col-xs-4">
-                                <h6>Business</h6>
-                                <p class="price">3 Site</p>
-                                <p class="price">&euro;99</p>
-                                <p>1 Year of Updates & Support**</p>
-                                <p>30 Days Satisfied or Refunded</p>
-                                <p>10 professional add-ons</p>
-                                <p><?php echo do_shortcode( '[purchase_link id="21345" text="Buy Now" style="button" direct="true"]' ) ?></p>
-                            </div>
-                            <div class="bundle third col-md-4 col-xs-4">
-                                <h6>Pro</h6>
-                                <p class="price">1 Unlimited Sites</p>
-                                <p class="price">&euro;199</p>
-                                <p>1 Year of Updates & Support**</p>
-                                <p>30 Days Satisfied or Refunded</p>
-                                <p>10 professional add-ons</p>
-                                <p><?php echo do_shortcode( '[purchase_link id="21347" text="Buy Now" style="button" direct="true"]' ) ?></p>
-                            </div>
-                        </div>
+                    <div class="container">
+	<div class="row">
+    			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+					<div class="panel price fist_box">
+						<h3>Personal</h3>
+						<ul class="list-group list-group-flush text-center">
+                                                        <li class="list-group-item mash_license">1 Site</li>
+							<li class="list-group-item mash_license">&euro;39</li>
+							<li class="list-group-item mash_etc">1 Year of Updates & Support**</li>
+							<li class="list-group-item mash_etc">30 Days Satisfied or Refunded</li>
+							<li class="list-group-item mash_etc">10 professional add-ons</li>
+							<li class="list-group-item mash_btn"> <?php echo do_shortcode( '[purchase_link id="21330" text="Buy Now" style="button" direct="true"]' ) ?></li>
+						</ul>
+					</div>
+			</div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+					<div class="panel price second_box">
+						<h3>Personal</h3>
+						<ul class="list-group list-group-flush text-center">
+                                                        <li class="list-group-item mash_license">3 Site</li>
+							<li class="list-group-item mash_license">&euro;99</li>
+							<li class="list-group-item mash_etc">1 Year of Updates & Support**</li>
+							<li class="list-group-item mash_etc">30 Days Satisfied or Refunded</li>
+							<li class="list-group-item mash_etc">10 professional add-ons</li>
+							<li class="list-group-item mash_btn"> <?php echo do_shortcode( '[purchase_link id="21330" text="Buy Now" style="button" direct="true"]' ) ?></li>
+						</ul>
+					</div>
+			</div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+					<div class="panel price third_box">
+						<h3>Personal</h3>
+						<ul class="list-group list-group-flush text-center">
+                                                        <li class="list-group-item mash_license">Unlimited Sites</li>
+							<li class="list-group-item mash_license">&euro;199</li>
+							<li class="list-group-item mash_etc">1 Year of Updates & Support**</li>
+							<li class="list-group-item mash_etc">30 Days Satisfied or Refunded</li>
+							<li class="list-group-item mash_etc">10 professional add-ons</li>
+							<li class="list-group-item mash_btn"> <?php echo do_shortcode( '[purchase_link id="21330" text="Buy Now" style="button" direct="true"]' ) ?></li>
+						</ul>
+					</div>
+			</div>
+			</div>
+            
+</div>
                     </div><!-- end container -->
 		</div> <!-- end content -->
             </div><!-- end row -->
