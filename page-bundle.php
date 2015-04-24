904b0f518bf6db7d9f49592e69c56657bc1097b7
Steffen Arnold <st.arnold.84@googlemail.com>
2015-04-24 15:11:30 +0200

    page-budnle.php

--- page-bundle.php
+++ page-bundle.php
@@ -14,7 +14,7 @@ Template Name: page-bundle
     <div class="row"><!--start row-->
         <div class="col-md-12" style="text-align: center;">
             <h1>30 Day Money Back Guarantee</h1>
-            <h2>We stand behind our product 100% – see our refund policy</h2>
+            <h2>We stand behind our product 100% – see our <a href="<?php echo get_permalink('21306'); ?>">refund policy</a></h2>
         </div>
     </div><!--end row-->
 </div><!--end header-->
@@ -46,7 +46,7 @@ Template Name: page-bundle
 							<li class="list-group-item mash_etc">1 Year of Updates & Support**</li>
 							<li class="list-group-item mash_etc">30 Days Satisfied or Refunded</li>
 							<li class="list-group-item mash_etc">10 professional add-ons</li>
-							<li class="list-group-item mash_btn"> <?php echo do_shortcode( '[purchase_link id="21330" text="Buy Now" style="button" direct="true"]' ) ?></li>
+							<li class="list-group-item mash_btn"> <?php echo do_shortcode( '[purchase_link id="21345" text="Buy Now" style="button" direct="true"]' ) ?></li>
 						</ul>
 					</div>
 			</div>
@@ -59,13 +59,62 @@ Template Name: page-bundle
 							<li class="list-group-item mash_etc">1 Year of Updates & Support**</li>
 							<li class="list-group-item mash_etc">30 Days Satisfied or Refunded</li>
 							<li class="list-group-item mash_etc">10 professional add-ons</li>
-							<li class="list-group-item mash_btn"> <?php echo do_shortcode( '[purchase_link id="21330" text="Buy Now" style="button" direct="true"]' ) ?></li>
+							<li class="list-group-item mash_btn"> <?php echo do_shortcode( '[purchase_link id="21347" text="Buy Now" style="button" direct="true"]' ) ?></li>
 						</ul>
 					</div>
 			</div>
 			</div>
             
 </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12" style="text-align: center;">
+                                <p>*A license allows to install and use Mashshare and all Add-Ons on a site for an unlimited period of time. This is not a subscription!<br />
+**To continue to receive support and updates, all licenses must be renewed after one year (not compulsory).</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="container mash_police">
+                        <div class="row">
+                            <div class="mash_bundle col-md-6 col-sm-12 col-xs-12">
+                                    <h6>Do I need to renew my license?</h6>
+                                    <p>Your license key is valid for one year from the purchase date. You need an active license key for continued access to automatic updates and support. If you renew your license each year you’ll receive a huge 40% discount off the current price!</p>  
+                            </div>
+                            <div class="mash_bundle col-md-6 col-sm-12 col-xs-12">
+                                    <h6>Do you have a refund policy?</h6>
+                                    <p><a href="<?php echo get_permalink('21306'); ?>" target="_blank">Yes we do</a>! We stand behind the quality of our product and will refund 100% of your money if you are unhappy with the plugin.</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="mash_bundle col-md-6 col-sm-12 col-xs-12">
+                                    <h6>Do I get access to add-ons?</h6>
+                                    <p>With your payment you can download <a href="<?php echo get_permalink('477'); ?>" target="_blank">10 professional add-ons</a>. This add-ons can be also downloaded from your purchase account.</p>
+                            </div>
+                            <div class="mash_bundle col-md-6 col-sm-12 col-xs-12">
+                                    <h6>Do I get updates for the plugin?</h6>
+                                    <p>Yes! Automatic updates are available free of charge to all users with a valid license key.</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="mash_bundle col-md-6 col-sm-12 col-xs-12">
+                                    <h6>Can I purchase the add-ons separately?</h6>
+                                    <p>Yes you can purchase the add-ons separately.</p>
+                            </div>
+                            <div class="mash_bundle col-md-6 col-sm-12 col-xs-12">
+                                    <h6>Do you offer support if I need help?</h6>
+                                    <p>Yes! Top-notch customer support is key for a quality product, so we’ll do our very best to resolve any issues you encounter via our support page.</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="mash_bundle col-md-6 col-sm-12 col-xs-12">
+                                    <h6>I have other pre-sale questions, can you help?</h6>
+                                    <p>Yes! You can ask us any question through our support page.</p>
+                            </div>
+                        </div>
+                        
+                        
+                    </div>
                     </div><!-- end container -->
 		</div> <!-- end content -->
             </div><!-- end row -->
