e90a0f720a21664f156624dc3cc8929bce32f849
René Hermenau <admin@xsimulator.net>
2015-04-25 14:24:15 +0200

    changes

--- functions.php
+++ functions.php
@@ -336,3 +336,32 @@ if ($terms) {
   }
 }
 }
+
+/* A special widget which is not shown when its content is empty
+Use ful when widgets should be only visible for non logged in user
+*/
+
+function mashDiscreetText() {
+class mashDiscreetTextWidget extends WP_Widget_Text
+{
+	function mashDiscreetTextWidget() {
+		$widget_ops = array('classname' => 'discreet_text_widget', 'description' => __('Arbitrary text or HTML, only shown if not empty'));
+		$control_ops = array('width' => 400, 'height' => 350);
+		$this->WP_Widget('discrete_text', __('Discreet Text'), $widget_ops, $control_ops);
+	}
+
+	function widget( $args, $instance ) {
+		extract($args, EXTR_SKIP);
+		$text = apply_filters( 'widget_text', $instance['text'] );
+		if (empty($text)) return;
+		$title = apply_filters('widget_title', empty($instance['title']) ? '' : $instance['title']);
+		echo $before_widget;
+		if ( !empty( $title ) ) { echo $before_title . $title . $after_title; } ?>
+			<div class="textwidget"><?php echo $instance['filter'] ? wpautop($text) : $text; ?></div>
+		<?php
+		echo $after_widget;
+	}
+}
+return register_widget("mashDiscreetTextWidget");
+}
+add_action('widgets_init', 'mashDiscreetText');
---
+++ page-sidebar-right.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Template Name: page-sidebar-right
+*/
+?>
+
+<?php get_header(); ?>
+
+	<div id="page-wrap" class="container-fluid">
+            <div id="header-image"><!--start header-image-->
+<div id="header-content" class="container" style="padding-bottom:40px;"><!--start header-->
+    <div class="row"><!--start row-->
+        <?php if(has_post_thumbnail()){?>
+        <div class="col-md-8">
+            <h1><?php the_title(); ?></h1>
+            <h2><?php the_excerpt();?></h2>
+        </div>
+        <div class="col-md-4">
+            <?php the_post_thumbnail(); ?>
+        </div>
+        <?php } else { ?>
+        <div class="col-xs-12 col-md-offset-2 col-md-8" style="text-align:center;">
+            <h1><?php the_title(); ?></h1>
+            <h2><?php the_excerpt();?></h2>
+        </div>
+        <?php } ?>
+    </div><!--end row-->
+</div><!--end header-->
+</div><!--end header-image-->
+<div class="decor-top container"><img src="<?php echo get_template_directory_uri(); ?>/images/decor_top.png" alt="blue" /></div>
+            <div class="row"><!-- start row -->
+		<div class="col-xs-12 col-sm-6 col-md-offset-2 col-md-6 col-lg-6">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="entry">
+					<?php the_content(); ?>
+				</div>
+                        </article>
+			<?php endwhile; endif; ?>
+		</div> <!-- end content -->
+                <div id="secondary" class="col-xs-12 col-sm-2 col-md-2 col-lg-2" style="margin-top:30px;"><!-- Sidebar right -->
+                    <div style="min-width: 250px;">
+                    <?php dynamic_sidebar( 'support-forum' ); ?>
+                    </div>
+                </div> <!-- end Sidebar right -->
+            </div><!-- end row -->
+	</div> <!-- end page-wrap -->
+<?php do_action( 'rootstrap_post_after' ); ?>
+<?php get_footer(); ?>
