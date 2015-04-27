b603a0254fb6a8696e7071f047242e5c05525892
René Hermenau <admin@xsimulator.net>
2015-04-27 21:04:18 +0200

    new

--- functions.php
+++ functions.php
@@ -388,3 +388,12 @@ class mashDiscreetTextWidget extends WP_Widget_Text
 return register_widget("mashDiscreetTextWidget");
 }
 add_action('widgets_init', 'mashDiscreetText');
+
+/* Prevent tinyMce from removing span tags */
+
+function override_mce_options($initArray) {
+$opts = '*[*]';
+$initArray['valid_elements'] = $opts;
+$initArray['extended_valid_elements'] = $opts;
+return $initArray;} 
+add_filter('tiny_mce_before_init', 'override_mce_options');
