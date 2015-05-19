ab498e43f51af7be08161719225209e0b4c15590
Steffen Arnold <st.arnold.84@googlemail.com>
2015-05-19 23:26:35 +0200

    404 new layout

--- 404.php
+++ 404.php
@@ -1,56 +1,32 @@
 <?php
-/**
- * The template for displaying 404 pages (Not Found).
- *
- * @package Mashshare
- */
+/*
+Template Name: page-faq
+*/
+?>
 
-get_header(); ?>
-	<div id="content" class="site-content container content-404">
-		<div id="primary" class="">
-			<main id="main" class="site-main" role="main">
+<?php get_header(); ?>
 
-				<section class="error-404 not-found">
-					<header class="page-header">
-						<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'rootstrap' ); ?></h1>
-					</header><!-- .page-header -->
+	
 
-					<div class="page-content">
-						<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'rootstrap' ); ?></p>
-
-						<?php get_search_form(); ?>
-
-						<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-						<?php if ( rootstrap_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-						<div class="widget widget_categories">
-							<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'rootstrap' ); ?></h2>
-							<ul>
-							<?php
-								wp_list_categories( array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								) );
-							?>
-							</ul>
-						</div><!-- .widget -->
-						<?php endif; ?>
-
-						<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'rootstrap' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-						?>
-
-						<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-					</div><!-- .page-content -->
-				</section><!-- .error-404 -->
-
-			</main><!-- #main -->
-		</div><!-- #primary -->
-
+	<div id="page-wrap" class="container-fluid">
+            <div id="header-image"><!--start header-image-->
+<div id="header-content" class="container"><!--start header-->
+    <div class="row"><!--start row-->
+        <div class="col-md-12">
+            <h1>404</h1>
+            
+        </div>
+        
+    </div><!--end row-->
+</div><!--end header-->
+</div><!--end header-image-->
+<div class="decor-top container"><img src="<?php echo get_template_directory_uri(); ?>/images/decor_top.png" alt="blue" /></div>
+            <div class="row"><!-- start row -->
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <p>Oops! That page can’t be found</p>
+		</div> <!-- end content -->
+            </div><!-- end row -->
+	</div> <!-- end page-wrap -->
+			<?php do_action( 'rootstrap_post_after' ); ?>
+		<?php get_sidebar( 'home' ); ?>
 <?php get_footer(); ?>
