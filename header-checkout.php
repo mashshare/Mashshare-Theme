1f2b26c60316ce7ccd37003f1a312a68152d6d98
René Hermenau <admin@xsimulator.net>
2018-04-18 12:08:42 +0200

    anonymize GA tag

--- header-checkout.php
+++ header-checkout.php
@@ -23,16 +23,16 @@
 <!--[if IE]><?php if ( rootstrap_get_option( 'custom_favicon' ) ) { ?><link rel="shortcut icon" href="<?php echo rootstrap_get_option( 'custom_favicon' ); ?>" /><?php } ?><![endif]-->	
 
 <?php wp_head(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-912289-17"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  ga('create', 'UA-912289-17', 'mashshare.net');
-  ga('require', 'displayfeatures');
-  ga('send', 'pageview');
+  gtag('config', 'UA-912289-17');
 </script>
+
 </head>
 
 <body <?php body_class(); ?>>
